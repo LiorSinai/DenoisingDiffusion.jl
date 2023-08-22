5fbefc8cc286ad5d5314b9c64cbce141b516ad8c
Lior Sinai <sinailior@gmail.com>
2023-08-22 22:47:35 +0200

    Rename test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,9 @@ using Test
 using Flux
 
 @testset verbose = true "DenoisingDiffusion" begin
-    include("test_models.jl")
+    include("models.jl")
     include("convert_fixed_nested.jl")
-    include("test_attention.jl")
+    include("attention.jl")
     include("ddim.jl")
     include("split_validation.jl")
 end
