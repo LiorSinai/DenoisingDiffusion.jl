2eb9f80c69c6f43e7af1ddf1fc4602cf7e4a85ff
Lior Sinai <sinailior@gmail.com>
2023-08-22 21:48:14 +0200

    use LaTeX format in docstring

--- examples/gaussian_frechet_distance.jl
+++ examples/gaussian_frechet_distance.jl
@@ -1,10 +1,10 @@
 """
     gaussian_frechet_distance(μ1, Σ1, μ2, Σ2)
 
-The Frechet distance between two multivariate Gaussians `X_1 ~ N(μ1, Σ1)`
-and `X_2 ~ N(μ2, Σ2)` is
-    
-    d^2 = ||μ1 - μ2||^2 + tr(Σ1 + Σ2 - 2*sqrt(Σ1*Σ2))
+The Frechet distance between two multivariate Gaussians ``X_1 ~ N(μ1, Σ1)``
+and ``X_2 ~ N(μ2, Σ2)`` is
+
+``d^2 = ||μ1 - μ2||^2 + tr(Σ1 + Σ2 - 2*\\sqrt{Σ1*Σ2})``
 """
 function gaussian_frechet_distance(μ1::AbstractMatrix, Σ1::AbstractMatrix, μ2::AbstractMatrix, Σ2::AbstractMatrix)
     diff = μ1 - μ2
