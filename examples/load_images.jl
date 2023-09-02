12169b316de64cfaf8801bcc6e1889f6978ddfad
Lior Sinai <sinailior@gmail.com>
2023-09-02 11:31:33 +0200

    remove dependency on Flux.batch. The load_images function is now about 15% faster. Also, it will raise an error immediately if one image does not conform to the desired size instead of only at the batching stage.

--- examples/load_images.jl
+++ examples/load_images.jl
@@ -1,4 +1,3 @@
-using Flux: batch
 using ProgressMeter
 using Images
 
@@ -7,14 +6,19 @@ function load_images(directory::AbstractString)
     get_pos(filename::String) = parse(Int, match(r"\d+", filename).match)
     sort!(filenames, lt=(s1, s2)-> get_pos(s1) < get_pos(s2));
 
-    xs = Array{Float32, 3}[]
-    @showprogress for filename in filenames
+    img1 = load(joinpath(directory, filenames[1]))
+    img1_WHC = convert_width_height_channel(img1)
+    nbatch = length(filenames)
+    X = Array{Float32}(undef, size(img1_WHC)..., nbatch)
+    @showprogress for (idx, filename) in enumerate(filenames)
         img = load(joinpath(directory, filename));
-        img_CHW = channelview(img)
-        img_WHC = permutedims(img_CHW, (3, 2, 1))
-        x = Float32.(img_WHC)
-        push!(xs, x)
+        X[:, :, :, idx] = convert_width_height_channel(img)
     end
-    X = batch(xs);
     X
+end
+
+function convert_width_height_channel(img::AbstractMatrix)
+    img_CHW = channelview(img)
+    img_WHC = permutedims(img_CHW, (3, 2, 1))
+    img_WHC
 end
